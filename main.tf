2dc903bbe6f73ea965d864caeea9b14ca4ab9dd1
jshenaex <john.shen@aexp.com>
2022-04-05 12:28:10 -0400

    another commit

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-064ff912f78e3e561"
   instance_type = "t2.micro"
 
   tags = {
